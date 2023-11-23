9b181c776c5961ae4e09af257c53cbd4e96e09c2
mehdi-mtir <mehdi.mtir@gmail.com>
2023-11-23 09:33:34 +0100

    incrementByValue completed

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,17 +1,31 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, incrementByValue } from "./counterSlice";
+import { useState } from "react";
 
 const Counter = ()=>{
   const count = useSelector((state)=>state.counter.count);
+  const [incrementValue, setIncrementValue] = useState(0);
+
+  const onIncrementValueChange = ({target})=>{
+    setIncrementValue(Number(target.value) || 0);
+  }
 
   const dispatch = useDispatch();
 
   return <>
         <h1>Counter App</h1>
         <p>{count}</p>
+        <div>
+          <input
+            type="number"
+            name="icrementValue"
+            value={incrementValue}
+            onChange = {onIncrementValueChange}/>
+        </div>
+
         <button onClick={()=>dispatch(increment())} className='btn btn-success'>+</button>
         <button onClick={()=>dispatch(decrement())} className='btn btn-danger'>-</button>
-        <button onClick={()=>dispatch(incrementByValue(2))} className="btn btn-primary"> +2 </button>
+        <button onClick={()=>dispatch(incrementByValue(incrementValue))} className="btn btn-primary"> +incrementValue </button>
   </>
 }
 
